235349337f11e212b07fbe0ca35efaeb08261685
elior99 <46793021+elior99@users.noreply.github.com>
2023-06-24 11:18:19 +0300

    update cors 3

--- server/index.js
+++ server/index.js
@@ -9,11 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-app.use(
-  cors({
-    origin: ["https://taskerweb.vercel.app"], // Add your frontend URLs here
-  })
-);
+app.use( cors());
 
 app.use(express.json());
 
